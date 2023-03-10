8c4bf0f0a013906b2d4c0609e8f19790ea2cc138
Jan Sambor <u45_jansam_waw@tehcnischools.com>
2023-03-10 15:14:48 +0100

    test

--- app.py
+++ app.py
@@ -9,11 +9,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/main')
+def All():
+    return render_template('main.html')
+
+
 @app.route('/historia')
 def historia():  # put application's code here
     file = open('./static/Polska 17w.md', 'r')
     md_template_string = markdown.markdown(file.read())
     return md_template_string
+
+
+@app.route('/biologia')
+def biologia():  # put application's code here
+    return render_template('biologia.html')
 
 
 if __name__ == '__main__':
